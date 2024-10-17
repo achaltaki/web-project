8ccde7f6806292e92182919188250281d7d0c2b3
achaltaki <156675427+achaltaki@users.noreply.github.com>
2024-10-17 13:33:16 +0530

    Update main.js

--- target-site6/main.js
+++ target-site6/main.js
@@ -1,16 +1,16 @@
 //today forcast is 273 kelvin
 // kelvin value remain constant so we use "const"  to define variable 
-const kelvin=273;
+const kelvin=293;
 // celsius is 273 degree less than kelvin
 //sub 273 from kelvin to get celsius value
 const celsius=kelvin-273;
 //use the equation to convert celsius to fahrenhiet
 //fahrenhiet= celsius*(9/5)+32
 //we use "let" because we will modify fahrenhiet value
-let fahrenhiet=celsius*(9/5)+32;
-fahrenhiet=math.floor(fahrenhiet);
-console.log('The Temperature is ${fahrenhiet} degrees Fahrenhiet.');
+let fahrenheit=celsius*(9/5)+32;
+fahrenheit=Math.floor(fahrenheit);
+console.log(`The Temperature is ${fahrenheit} degrees Fahrenheit.`);
 let newton=celsius*(33/100);
-newton=math.floor(newton);
-console.log('The Temperature is ${newton} degrees Newton.');
+newton=Math.floor(newton);
+console.log(`The Temperature is ${newton} degrees Newton.`);
 
